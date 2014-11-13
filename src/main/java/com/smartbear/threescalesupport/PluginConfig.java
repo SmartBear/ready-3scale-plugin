60c3786124367563b670555f76daf2f5a2d91254
Roman-Rostovtsev <Roman.Rostovtsev@smartbear.com>
2014-11-13 15:52:15 +0300

    The plugin must not be installed to RAPI 1.0.0

--- src/main/java/com/smartbear/threescalesupport/PluginConfig.java
+++ src/main/java/com/smartbear/threescalesupport/PluginConfig.java
@@ -6,7 +6,7 @@ import com.eviware.soapui.plugins.PluginDependency;
 
 @PluginConfiguration(groupId = "com.smartbear.plugins", name = "ready-3scale-plugin", version = "1.0",
         autoDetect = true, description = "3Scale Support Plugin",
-        infoUrl = "" )
+        infoUrl = "", minimumReadyApiVersion = "1.0.1", minimumSoapUIVersion = "5.2")
 @PluginDependency(groupId = "com.smartbear.soapui.plugins", name = "Swagger Plugin", minimumVersion = "2.0")
 public class PluginConfig extends PluginAdapter {
 }
