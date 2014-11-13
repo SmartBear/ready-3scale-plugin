ecf8a68cf97798b13add3b8c06f148db3ec8155c
Roman-Rostovtsev <Roman.Rostovtsev@smartbear.com>
2014-11-13 12:26:25 +0300

    Made the plugin depending from Swagger plugin (excluded its classes from jar)

--- src/main/java/com/smartbear/threescalesupport/PluginConfig.java
+++ src/main/java/com/smartbear/threescalesupport/PluginConfig.java
@@ -2,9 +2,11 @@ package com.smartbear.threescalesupport;
 
 import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
+import com.eviware.soapui.plugins.PluginDependency;
 
 @PluginConfiguration(groupId = "com.smartbear.plugins", name = "ready-3scale-plugin", version = "1.0",
         autoDetect = true, description = "3Scale Support Plugin",
         infoUrl = "" )
+@PluginDependency(groupId = "com.smartbear.soapui.plugins", name = "Swagger Plugin", minimumVersion = "2.0")
 public class PluginConfig extends PluginAdapter {
 }
