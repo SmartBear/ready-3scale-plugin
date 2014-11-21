6f9bebe512c47c39e04103ac8411abccf12c852f
Ole Lensmar <ole.lensmar@smartbear.com>
2014-11-21 14:21:59 -0500

    added infoUrl to PluginInfo

--- src/main/java/com/smartbear/threescalesupport/PluginConfig.java
+++ src/main/java/com/smartbear/threescalesupport/PluginConfig.java
@@ -6,7 +6,7 @@ import com.eviware.soapui.plugins.PluginDependency;
 
 @PluginConfiguration(groupId = "com.smartbear.plugins", name = "ready-3scale-plugin", version = "1.0",
         autoDetect = true, description = "3Scale Support Plugin",
-        infoUrl = "", minimumReadyApiVersion = "1.0.1", minimumSoapUIVersion = "5.2")
+        infoUrl = "https://github.com/SmartBear/ready-3scale-plugin", minimumReadyApiVersion = "1.0.1", minimumSoapUIVersion = "5.2")
 @PluginDependency(groupId = "com.smartbear.soapui.plugins", name = "Swagger Plugin", minimumVersion = "2.0")
 public class PluginConfig extends PluginAdapter {
 }
