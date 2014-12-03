bc99f2c3ab69d9ce27a3786f96ba690324151d5b
Ole Lensmar <ole.lensmar@smartbear.com>
2014-12-03 08:40:01 -0500

    updated version to 1.0.1

--- src/main/java/com/smartbear/threescalesupport/PluginConfig.java
+++ src/main/java/com/smartbear/threescalesupport/PluginConfig.java
@@ -3,7 +3,7 @@ package com.smartbear.threescalesupport;
 import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
 
-@PluginConfiguration(groupId = "com.smartbear.plugins", name = "3scale Plugin", version = "1.0",
+@PluginConfiguration(groupId = "com.smartbear.plugins", name = "3scale Plugin", version = "1.0.1",
         autoDetect = true, description = "Adds actions to import APIs from 3scale hosted developer portals",
         infoUrl = "https://github.com/SmartBear/ready-3scale-plugin" )
 public class PluginConfig extends PluginAdapter {
